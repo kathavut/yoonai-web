7c6864eb4f5f90cb7b52801b86b9f8b562958b40
kathavut <t.kathavut@gmail.com>
2018-09-12 14:52:16 +0700

    build-tools [production]

--- app/Config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  DB: 'mongodb://localhost:27017/yoonai',
-  HOST_NAME:'localhost',
-  API:'http://localhost:4000/api',
-  APP_PORT: 4000
-};
--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 //server.js
 /* jslint node: true */
 'use strict';
-<<<<<<< HEAD
 var compression = require('compression');
-=======
->>>>>>> 05650b7624ce288a671cfdf34f284b285050d235
 var express = require('express');
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
@@ -13,7 +10,6 @@ var morgan = require('morgan');
 var path = require('path');
 var hbs = require('express-handlebars');
 
-<<<<<<< HEAD
 // Require configuration file defined in app/Config.js
 var config = require('./app/config');
 
@@ -23,12 +19,6 @@ app.use(compression());
 //view engine setup
 app.engine('hbs',hbs({extname:'hbs',defaultLayout:'layout',layoutsDir:__dirname+'/views/layouts/'}));
 app.set('views',path.join(__dirname,'views'));
-=======
-var app = express();
-//view engine setup
-app.engine('hbs',hbs({extname:'hbs',defaultLayout:'layout',layoutsDir:__dirname+'/public/layouts/'}));
-app.set('views',path.join(__dirname,'public'));
->>>>>>> 05650b7624ce288a671cfdf34f284b285050d235
 app.set('view engine','hbs');
 
 app.use(cookieParser());
@@ -37,16 +27,11 @@ app.use(session({
     key: 'test',
     proxy: 'true',
     store: new MemcachedStore({
-<<<<<<< HEAD
         hosts: [process.env.MEMCACHE_URL || config.MEMCACHE_URL[0]]
-=======
-        hosts: [process.env.MEMCACHE_URL || '127.0.0.1:12321']
->>>>>>> 05650b7624ce288a671cfdf34f284b285050d235
     })
 }));
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-<<<<<<< HEAD
 // Connect to database
 mongoose.connect(config.DB);
 // Sends static files  from the public path directory
@@ -54,16 +39,6 @@ app.use(express.static(path.join(__dirname, '/views')));
 // Use morgan to log request in dev mode
 app.use(morgan('dev'));
 //app.use(morgan(':remote-addr - :remote-user [:date[iso]] ":method :url HTTP/:http-version" :status :res[content-length]'));
-=======
-// Require configuration file defined in app/Config.js
-var config = require('./app/Config');
-// Connect to database
-mongoose.connect(config.DB);
-// Sends static files  from the public path directory
-app.use(express.static(path.join(__dirname, '/public')));
-// Use morgan to log request in dev mode
-app.use(morgan('dev'));
->>>>>>> 05650b7624ce288a671cfdf34f284b285050d235
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -71,11 +46,7 @@ var port = config.APP_PORT || 4000;
 app.listen(port); // Listen on port defined in config file
 console.log('App listening on port ' + port);
 
-<<<<<<< HEAD
 var apiRoutes = require('./app/routes');
-=======
-var apiRoutes = require('./app/Routes');
->>>>>>> 05650b7624ce288a671cfdf34f284b285050d235
 //  Use routes defined in Route.js and prefix it with api
 app.use('/api', apiRoutes);
 
@@ -95,11 +66,7 @@ app.use(function (req, res, next) {
   next();
 });
 // Server index.html page when request to the root is made
-<<<<<<< HEAD
 app.get('/', function (req, res) {
-=======
-app.get('/', function (req, res, next) {
->>>>>>> 05650b7624ce288a671cfdf34f284b285050d235
     res.render('index',{
         title:'ok-hbs',
         msg:'สวัสดีไทยแลนด์',
@@ -107,11 +74,7 @@ app.get('/', function (req, res, next) {
         _session:JSON.stringify(req.session)
     });
 });
-<<<<<<< HEAD
 app.get('/signup',function(req,res){
-=======
-app.get('/signup',function(req,res,next){
->>>>>>> 05650b7624ce288a671cfdf34f284b285050d235
     res.render('index',{
         title:'ok-hbs',
         msg:'สวัสดีไทยแลนด์',
@@ -119,11 +82,7 @@ app.get('/signup',function(req,res,next){
         _session:JSON.stringify(req.session)
     });
 });
-<<<<<<< HEAD
 app.get('/login',function(req,res){
-=======
-app.get('/login',function(req,res,next){
->>>>>>> 05650b7624ce288a671cfdf34f284b285050d235
     res.render('index',{
         title:'#login : Yoonai',
         msg:'สวัสดีไทยแลนด์',
